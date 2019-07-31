e1e5220ea3142900355a84751319dc103fd0a4d3
tuhalang <tuhalang007@outlook.com.vn>
2019-08-01 02:48:17 +0700

    comming soon

--- auto_register.py
+++ auto_register.py
@@ -5,25 +5,13 @@ from utils import stringToRGB
 from decode_captcha import decode
 import signal
 from contextlib import contextmanager
-
-class TimeoutException(Exception): pass
-
-@contextmanager
-def time_limit(seconds):
-    def signal_handler(signum, frame):
-        raise TimeoutException("Timed out!")
-    signal.signal(signal.SIGALRM, signal_handler)
-    signal.alarm(seconds)
-    try:
-        yield
-    finally:
-        signal.alarm(0)
+from selenium.webdriver.common.keys import Keys
 
 
 USERNAME = ""
 PASSWORD = ""
 URL = "https://dk-sis.hust.edu.vn/Users/Login.aspx"
-subjects = ['1234','2345']
+subjects = ['112363','688084','111559','111569','111570']#'111549','689529','111565','689511',
 
 PATH_TO_CHROMEDRIVER = "/home/tuhalang/Documents/Python/auto_register/chromedriver"
 
@@ -36,7 +24,7 @@ def login():
     # start comment
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
-    driver = webdriver.Chrome(executable_path=PATH_TO_CHROMEDRIVER,chrome_options=options)
+    driver = webdriver.Chrome(executable_path=PATH_TO_CHROMEDRIVER)#,chrome_options=options)
     # end comment
 
     #driver = webdriver.Chrome(executable_path=PATH_TO_CHROMEDRIVER)
@@ -46,46 +34,57 @@ def login():
     driver.get(URL)
     time.sleep(0.5)
 
-    driver.find_element_by_xpath('//*[@id="tbUserName"]').send_keys(USERNAME)
+    
     # time.sleep(0.5)
 
     current_url = URL
     while current_url == URL:
         captcha_code = ""
         try:
-            with time_limit(1):
-                ele_captcha = driver.find_element_by_xpath(
-                    '//*[@id="ccCaptcha_IMG"]')
-                # get the captcha as a base64 string
-                img_captcha_base64 = driver.execute_async_script("""
-                        var ele = arguments[0], callback = arguments[1];
-                        ele.addEventListener('load', function fn(){
-                        ele.removeEventListener('load', fn, false);
-                        var cnv = document.createElement('canvas');
-                        cnv.width = this.width; cnv.height = this.height;
-                        cnv.getContext('2d').drawImage(this, 0, 0);
-                        callback(cnv.toDataURL('image/png').substring(22));
-                        }, false);
-                        ele.dispatchEvent(new Event('load'));
-                        """, ele_captcha)
-                image = stringToRGB(img_captcha_base64)
-                captcha_code = decode(image, model)
+            driver.find_element_by_xpath('//*[@id="tbUserName"]').clear()
+            driver.find_element_by_xpath('//*[@id="tbUserName"]').send_keys(USERNAME)
+            ele_captcha = driver.find_element_by_xpath(
+                '//*[@id="ccCaptcha_IMG"]')
+            # get the captcha as a base64 string
+            img_captcha_base64 = driver.execute_async_script("""
+                    var ele = arguments[0], callback = arguments[1];
+                    ele.addEventListener('load', function fn(){
+                    ele.removeEventListener('load', fn, false);
+                    var cnv = document.createElement('canvas');
+                    cnv.width = this.width; cnv.height = this.height;
+                    cnv.getContext('2d').drawImage(this, 0, 0);
+                    callback(cnv.toDataURL('image/png').substring(22));
+                    }, false);
+                    ele.dispatchEvent(new Event('load'));
+                    """, ele_captcha)
+            image = stringToRGB(img_captcha_base64)
+            captcha_code = decode(image, model)
+            driver.find_element_by_xpath('//*[@id="tbPassword_CLND"]').click()
+            driver.find_element_by_xpath(
+            '//*[@id="tbPassword"]').send_keys(PASSWORD)
+            driver.find_element_by_xpath(
+            '//*[@id="ccCaptcha_TB_I"]').send_keys(captcha_code)
+            driver.find_element_by_xpath('//*[@id="ctl01"]/p[4]/button').click()
+            time.sleep(0.5)
+            current_url = driver.current_url
         except Exception as e:
             print(e)
-        driver.find_element_by_xpath('//*[@id="tbPassword_CLND"]').click()
-        driver.find_element_by_xpath(
-            '//*[@id="tbPassword"]').send_keys(PASSWORD)
-        driver.find_element_by_xpath(
-            '//*[@id="ccCaptcha_TB_I"]').send_keys(captcha_code)
-        driver.find_element_by_xpath('//*[@id="ctl01"]/p[4]/button').click()
-        time.sleep(0.5)
-        current_url = driver.current_url
+            driver.get(URL)
+            time.sleep(60)
+        
     return driver
 
 
 def main():
     driver = login()
-    pass
+    for subject in subjects:
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_ASPxSplitter1_Content_ContentSplitter_MainContent_ASPxCallbackPanel1_tbDirectClassRegister_I"]').send_keys(subject)
+        driver.find_element_by_xpath('//*[@id="ctl00_ctl00_ASPxSplitter1_Content_ContentSplitter_MainContent_ASPxCallbackPanel1_tbDirectClassRegister_I"]').send_keys(Keys.ENTER)
+        time.sleep(0.25)
+    driver.find_element_by_xpath('//*[@id="ctl00_ctl00_ASPxSplitter1_Content_ContentSplitter_MainContent_ASPxCallbackPanel1_btSendRegister"]').click()
+    driver.find_element_by_xpath('//*[@id="ctl00_ctl00_ASPxSplitter1_Content_ContentSplitter_MainContent_ASPxCallbackPanel1_pcYesNo_pcYesNoBody1_ASPxRoundPanel1_btnYes"]').click()
+    while True:
+        print("success")
 
 
 if __name__ == '__main__':
